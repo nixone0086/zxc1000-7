ec5aab7a7f54b9857615bb0f10f53bd46cabc33f
nekit <nekit@NIXONE>
2025-09-02 23:27:01 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSlight
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+
+           Console.WriteLine("Write: 1000-7 ");
+            string zxc = Console.ReadLine();
+            int b = 7;
+            int a = 1000;
+            if (zxc == "1000-7" || zxc == "1000 -7" || zxc == "1000 - 7" || zxc == "1000- 7")
+            {
+                while (a  > b)
+                {
+                    Console.WriteLine(a-b);
+                    a -= b;
+                }
+
+            }
+            Console.ReadKey();  
+       } 
+    }
+}
+
+ 
+
